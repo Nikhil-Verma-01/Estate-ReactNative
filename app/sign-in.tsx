f2901fab6c57d8b697a545429709be9ed19c665f
Nikhil-Verma-01 <nik.h0ill907@gmail.com>
2025-05-08 17:32:39 +0530

    Preparing ground for Implementation

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,11 +1,12 @@
-import { View, Text } from 'react-native'
+import { View, Text} from 'react-native'
 import React from 'react'
+//import { SafeAreaView } from 'react-native-safe-area-context'
 
 const SignIn = () => {
   return (
-    <View>
-      <Text >SignIn</Text>
-    </View>
+    //<SafeAreaView className='bg-white h-full'>
+      <Text>SignIn</Text>
+    //</SafeAreaView>
   )
 }
 
